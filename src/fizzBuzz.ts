f9032cd5f949f4437ad332c0b410c77418dee660
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 15:49:03 +0100

    refactor with generalization

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,8 +1,6 @@
 class FizzBuzz {
   printFizzBuzzOrNumber(numberValue: number): string {
-    if (numberValue === 2) return "2";
-    if (numberValue === 4) return "4";
-    return "1";
+    return numberValue.toString();
   }
 }
 
