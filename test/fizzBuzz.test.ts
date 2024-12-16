86231f8989e61e3c068563aa09a4b97b1a8697a6
Vito Alessandro Carella <berlinodeveloper@gmail.com>
2024-12-16 15:22:28 +0100

    rename test suite

--- test/fizzBuzz.test.ts
+++ test/fizzBuzz.test.ts
@@ -1,14 +1,11 @@
 import FizzBuzz from "../src/fizzBuzz";
 const FizzBuzzClass = new FizzBuzz();
 
-describe('sample test set', function (): void {
-
-  it('sample test', (): void => {
-
+describe("Fizz Buzz Test Suite", function (): void {
+  it("sample test", (): void => {
     var result: boolean = FizzBuzzClass.handle();
 
     var expected = true;
     expect(result).toBe(expected);
   });
-
 });
